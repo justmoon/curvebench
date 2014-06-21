e75439f50b2d69050a77982092a91cc192379800
Stefan Thomas <justmoon@members.fsf.org>
2014-06-21 14:18:24 -0700

    [TASK] Remove libgmp dependency.
    I compared secp256k1 using GMP vs OpenSSL and could not find any noticable difference.

--- src/curvebench/config.h
+++ src/curvebench/config.h
@@ -1,7 +1,7 @@
 #ifndef __CURVEBENCH_CONFIG_H
 #define __CURVEBENCH_CONFIG_H
 
-#define USE_NUM_GMP
+#define USE_NUM_OPENSSL
 #define USE_FIELD_5X52
 #define USE_FIELD_INV_NUM
 #define USE_FIELD_5X52_ASM
